0d8285e0fb0a50597720a74d0cc0938443a0c2cf
w-aikennedy <95069091+w-aikennedy@users.noreply.github.com>
2024-05-08 21:59:01 +0800

    Tidy up logging of response

--- app.py
+++ app.py
@@ -351,7 +351,7 @@ def conversation():
 
     if AZURE_APP_INSIGHTS_INSTRUMENTATION_KEY:
         tc = TelemetryClient(AZURE_APP_INSIGHTS_INSTRUMENTATION_KEY)
-        tc.track_event('Chatbot response', { 'response': str(conversation_internal_output.data) })
+        tc.track_event('Chatbot response', { 'response': conversation_internal_output.data.decode() })
         tc.flush()
     return conversation_internal_output
 
